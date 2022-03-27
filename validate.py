95392ae63c6927296a0757b70be9b563a45220b7
Yik Wee <yikweekoh@gmail.com>
2022-03-27 11:10:36 +0800

    Allow YYYY-MM-DD in date validation (html input format)

--- validate.py
+++ validate.py
@@ -54,42 +54,48 @@ def leap_year(yyyy: int) -> bool:
 def date(value: str) -> bool:
     """
     Return
-    - True if value is a valid str in YYYYMMDD format
+    - True if value is a valid str in YYYYMMDD format OR YYYY-MM-DD
     - False otherwise
     """
-    # TODO Fix date validation
-    # # Type check
-    # if not string(value):
-    #     return False
-    # # Length check
-    # if len(value) != 8:
-    #     return False
-    # # Format check
-    # if not value.isdigit():
-    #     return False
+    # Type check
+    if not string(value):
+        return False
+
+    # If YYYY-MM-DD format remove '-'
+    if value.count('-') != 2:
+        return False
+    value = value.replace('-', '')
+
+    # Length check
+    if len(value) != 8:
+        return False
+
+    # Format check
+    if not value.isdigit():
+        return False
     
-    # y, m, d = int(value[:4]), int(value[4:6]), int(value[6:])
+    y, m, d = int(value[:4]), int(value[4:6]), int(value[6:])
     
-    # # Format/Range check
-    # if not (1930 < y < 2030):
-    #     return False
-    # if not (1 <= m <= 12):
-    #     return False
-    # if (
-    #     m in (1, 3, 5, 7, 8, 10, 12)
-    #     and not (1 <= d <= 31)
-    # ):
-    #     return False
-    # if (
-    #     m in (4, 6, 9, 11)
-    #     and not (1 <= d <= 30)
-    # ):
-    #     return False
-    # if m == 2:
-    #     if leap_year(y) and not (1 <= d <= 29):
-    #         return False
-    #     elif not leap_year(y) and not (1 <= d <= 28):
-    #         return False
+    # Format/Range check
+    if not (1930 < y < 2030):
+        return False
+    if not (1 <= m <= 12):
+        return False
+    if (
+        m in (1, 3, 5, 7, 8, 10, 12)
+        and not (1 <= d <= 31)
+    ):
+        return False
+    if (
+        m in (4, 6, 9, 11)
+        and not (1 <= d <= 30)
+    ):
+        return False
+    if m == 2:
+        if leap_year(y) and not (1 <= d <= 29):
+            return False
+        elif not leap_year(y) and not (1 <= d <= 28):
+            return False
     return True
 
 def singapore_number(value: str) -> bool:
