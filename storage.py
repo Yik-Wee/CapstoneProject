59c457d02560dec111372a15bd5a79ce54595113
Yik Wee <yikweekoh@gmail.com>
2022-04-18 22:07:11 +0800

    Add docstrings, very minor refactoring

--- storage.py
+++ storage.py
@@ -1,26 +1,95 @@
+"""
+Storage classes to interface with the db.
+"""
 import sqlite3
 
+
 class Collection:
-    def __init__(self, db_name):
+    """
+    Storage base class to interface with the db
+
+    Attributes
+    ----------
+    column_names: List[str]
+    - The names of the columns in the table
+
+    db_path: str
+    - The path to the db file
+
+    Methods
+    -------
+    insert(record: dict) -> None
+    - Inserts a record into the table
+
+    find(filter: dict) -> dict
+    - Returns the records matching the filter in the table
+
+    update(filter: dict, new_record: dict) -> None
+    - Update the old record(s) matching `filter` to the `new_record` in the table
+
+    delete(filter: dict) -> None
+    - Deletes all records matching `filter` from the table
+    """
+
+    column_names = NotImplemented
+
+    def __init__(self, db_path: str):
+        """Initialise a Collection which interfaces with the db specified by `db_path`"""
         pass
 
-    def insert(self, record):
+    def insert(self, record: dict) -> None:
+        """
+        Insert a record into the db.
+        Record DOES NOT need to include the primary key if it is auto-incremented.
+        e.g. for club records
+        ```
+        record: {
+            'name': 'CLUB NAME'
+        }
+        ```
+        and
+        ```
+        record: {
+            'id': 10,
+            'name': 'CLUB NAME'
+        }
+        ```
+        are both accepted as the id for table `club` is AUTOINCREMENT-ed
+        """
         pass
 
-    def find(self, filter):
+    def find(self, filter: dict) -> dict:
+        """
+        Return all rows matching the `filter` specifications.
+        Return all columns from each record in the format
+        {
+            'column_1': ...,
+            'column_2': ...,
+        }
+        """
         pass
 
-    def update(self, filter, new_record):
+    def update(self, filter: dict, new_record: dict) -> None:
+        """
+        Update the old record(s) specified by `filter` with the `new_record`.
+        """
         pass
 
-    def delete(self, filter):
+    def delete(self, filter: dict) -> None:
+        """
+        Delete the records from the table matching the `filter`.
+        """
         pass
 
-class Students():
-    def __init__(self, db_name):
-        self.db_name = db_name
-        self.column_names = ['student_id', 'name', 'age', 'year_enrolled', 'graduating_year']
-        with sqlite3.connect(self.db_name) as conn:
+
+class Students(Collection):
+    # PLEASE BE CONSISTENT IS IT student_id OR id ?????????
+    # WHERE IS class_id YOU WROTE IT IN THE DATA SCHEMA AND ITS NOT HERE
+    column_names = ['student_id', 'name', 'age', 'year_enrolled', 'graduating_year']
+
+    def __init__(self, db_path):
+        self.db_path = db_path
+        with sqlite3.connect(self.db_path) as conn:
             c = conn.cursor()
             c.execute("""CREATE TABLE IF NOT EXISTS student(
                     student_id INTEGER, 
@@ -30,16 +99,17 @@ class Students():
                     graduating_year INTEGER,
                     PRIMARY KEY(student_id)
                      )""")
-
             conn.commit()
 
-    def insert(self, record: "dict") -> None:
-        with sqlite3.connect(self.db_name) as conn:
+    def insert(self, record: dict) -> None:
+        # TODO allow omission of `id` from record.
+        with sqlite3.connect(self.db_path) as conn:
             c = conn.cursor()
-            c.execute("""INSERT INTO student VALUES (?, ?, ?, ?, ?)""", list(record.values()))
+            c.execute("""INSERT INTO student VALUES (?, ?, ?, ?, ?)""",
+                      list(record.values()))
             conn.commit()
 
-    def find(self, filter: "dict") -> "dict":
+    def find(self, filter: dict) -> dict:
         """
         Return all rows matching the filter specifications.
         Return all columns from each record.
@@ -57,31 +127,30 @@ class Students():
             sql += f"{condition} = ? AND "
 
         sql = sql[:-4]
-        
-        with sqlite3.connect(self.db_name) as conn:
-            
+
+        with sqlite3.connect(self.db_path) as conn:
+
             c = conn.cursor()
             c.execute(f"""SELECT * FROM student
                       WHERE {sql} """, list(values))
             record = c.fetchone()
             return record
-            conn.commit()
-
-        
 
     def update(self, filter, new_record) -> None:
         # with sqlite3.connect(self.db_name) as conn:
         #     c = conn.cursor()
-        
+
         #     c.execute("""""")
-        pass    
+        pass
+
     def delete(self, filter):
         pass
-   
+
+
 class Subject(Collection):
     pass
 
-    
+
 class Clubs(Collection):
     pass
 
@@ -93,9 +162,18 @@ class Activities(Collection):
 class Classes(Collection):
     pass
 
-print('eorjgpoerg')
-people = Students('school')
-ren = {'student_id' : 1, 'name' : 'cassey', 'age' : 17, 'year_enrolled' : 2021, 'graduating_year' : 2022}
 
-people.insert(ren)
-print(people.find({'name' : 'cassey', 'age' : 17}))
+if __name__ == '__main__':
+    # testing code
+    print('eorjgpoerg')
+    people = Students('school')
+    ren = {
+        'student_id': 1,
+        'name': 'cassey',
+        'age': 17,
+        'year_enrolled': 2021,
+        'graduating_year': 2022
+    }
+
+    people.insert(ren)
+    print(people.find({'name': 'cassey', 'age': 17}))
