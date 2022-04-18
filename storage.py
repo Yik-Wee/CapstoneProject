d46561d8b7fd340a3628726b7d6ab75589056c5b
CASSEY LOW SI YUN LOW <98474258+caseeeee@users.noreply.github.com>
2022-04-18 06:33:16 +0000

    find method

--- storage.py
+++ storage.py
@@ -2,8 +2,6 @@ import sqlite3
 
 class Collection:
     def __init__(self, db_name):
-
-            
         pass
 
     def insert(self, record):
@@ -18,10 +16,10 @@ class Collection:
     def delete(self, filter):
         pass
 
-
-class Students(Collection):
+class Students():
     def __init__(self, db_name):
         self.db_name = db_name
+        self.column_names = ['student_id', 'name', 'age', 'year_enrolled', 'graduating_year']
         with sqlite3.connect(self.db_name) as conn:
             c = conn.cursor()
             c.execute("""CREATE TABLE IF NOT EXISTS student(
@@ -38,25 +36,48 @@ class Students(Collection):
     def insert(self, record: "dict") -> None:
         with sqlite3.connect(self.db_name) as conn:
             c = conn.cursor()
-            c.execute("""INSERT INTO student VALUES (?, ?, ?, ?, ?)""", record.values())
-            c.commit()
+            c.execute("""INSERT INTO student VALUES (?, ?, ?, ?, ?)""", list(record.values()))
+            conn.commit()
 
     def find(self, filter: "dict") -> "dict":
+        """
+        Return all rows matching the filter specifications.
+        Return all columns from each record.
+        """
+        # Check that filter keys are valid column names
+        for key, value in filter.items():
+            if key not in self.column_names:
+                raise KeyError(f"{key} is not a valid column name")
+
+        conditions = filter.keys()
+        values = filter.values()
+        sql = ''
+
+        for condition in conditions:
+            sql += f"{condition} = ? AND "
+
+        sql = sql[:-4]
+        
         with sqlite3.connect(self.db_name) as conn:
+            
             c = conn.cursor()
-            c.execute("""SELECT * FROM student
-                      WHERE """)
+            c.execute(f"""SELECT * FROM student
+                      WHERE {sql} """, list(values))
+            record = c.fetchone()
+            return record
+            conn.commit()
+
+        
 
     def update(self, filter, new_record) -> None:
-        with sqlite3.connect(self.db_name) as conn:
-            c = conn.cursor()
-            c.execute("""""")
-
-            
+        # with sqlite3.connect(self.db_name) as conn:
+        #     c = conn.cursor()
+        
+        #     c.execute("""""")
+        pass    
     def delete(self, filter):
         pass
-
-            
+   
 class Subject(Collection):
     pass
 
@@ -72,3 +93,9 @@ class Activities(Collection):
 class Classes(Collection):
     pass
 
+print('eorjgpoerg')
+people = Students('school')
+ren = {'student_id' : 1, 'name' : 'cassey', 'age' : 17, 'year_enrolled' : 2021, 'graduating_year' : 2022}
+
+people.insert(ren)
+print(people.find({'name' : 'cassey', 'age' : 17}))
